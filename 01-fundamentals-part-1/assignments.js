79fbe7bc9d43a14742c35f77f9745a31c794b3fe
abbypinkos <abbypinkos@gmail.com>
2021-11-02 15:16:13 -0500

    Complete Values and Variables

---
+++ 01-fundamentals-part-1/assignments.js
@@ -0,0 +1,7 @@
+let country = 'United States of America';
+let continent = 'North America';
+let population = '332,895,287';
+
+console.log(country);
+console.log(continent);
+console.log(population);
