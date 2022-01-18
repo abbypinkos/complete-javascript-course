e757ad19d37614000f56b735cdeeab7e042dcad8
abbypinkos <abbypinkos@gmail.com>
2022-01-18 16:51:19 -0600

    Complete Data Types

--- 01-fundamentals-part-1/assignments.js
+++ 01-fundamentals-part-1/assignments.js
@@ -1,3 +1,5 @@
+/*
+// LECTURE: Values and Variables
 let country = 'United States of America';
 let continent = 'North America';
 let population = '332,895,287';
@@ -5,3 +7,12 @@ let population = '332,895,287';
 console.log(country);
 console.log(continent);
 console.log(population);
+*/
+
+// LECTURE: Data Types
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
--- 01-fundamentals-part-1/script.js
+++ 01-fundamentals-part-1/script.js
@@ -1,8 +1,9 @@
+/*
 // Linking a JavaScript File
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
-//Values and Variables
+// Values and Variables
 console.log('Jonas');
 console.log(23);
 
@@ -24,3 +25,25 @@ let job1 = 'programmer';
 let job2 = 'teacher';
 
 console.log(myFirstJob);
+*/
+
+// Data Types
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
