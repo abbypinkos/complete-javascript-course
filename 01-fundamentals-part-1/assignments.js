ad8a9c096c71b93b8dc94b048409ac7fd0389f10
abbypinkos <abbypinkos@gmail.com>
2022-01-19 11:29:19 -0600

    Complete Basic Operators

--- 01-fundamentals-part-1/assignments.js
+++ 01-fundamentals-part-1/assignments.js
@@ -3,7 +3,7 @@
 // LECTURE: Values and Variables
 let country = 'United States of America';
 let continent = 'North America';
-let population = '332,895,287';
+let population = '332';
 
 console.log(country);
 console.log(continent);
@@ -17,7 +17,6 @@ console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
-*/
 
 ////////////////////////////////////
 // LECTURE: let, const and var
@@ -25,4 +24,19 @@ language = 'English';
 const country = 'United States of America';
 // Const cannot be reassinged; causes an error
 // country = 'Mexico'
-let population = '332,895,287';
+let population = '332';
+*/
+
+////////////////////////////////////
+// LECTURE: Basic Operators
+const country = 'The United States of America';
+const continent = 'North America';
+let population = '332';
+console.log(population / 2);
+population++;
+console.log(population);
+console.log(population > 6);
+console.log(population > 33);
+const language = 'English';
+const description = country + ' is in ' + continent + ', and its ' + population + ' million people speak ' + language;
+console.log(description);
