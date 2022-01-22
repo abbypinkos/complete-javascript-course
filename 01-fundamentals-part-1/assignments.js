50ef7574a1130acd9c70c0242d41eb506ff8eef9
abbypinkos <abbypinkos@gmail.com>
2022-01-21 22:26:03 -0600

    Complete JavaScript Fundamentals - Part 1

--- 01-fundamentals-part-1/assignments.js
+++ 01-fundamentals-part-1/assignments.js
@@ -39,4 +39,102 @@ console.log(population > 33);
 const language = 'English';
 const description = country + ' is in ' + continent + ', and its ' + population + ' million people speak ' + language;
 console.log(description);
+
+////////////////////////////////////
+// LECTURE: Strings and Template Literals
+const country = 'The United States of America';
+const continent = 'North America';
+let population = '333';
+const language = 'English';
+const description = `${country} is in ${continent}, and its ${population} million people speak ${language}.`;
+console.log(description);
+
+////////////////////////////////////
+// LECTURE: Taking Decisions: if / else Statements
+
+const country = 'The United States of America';
+let population = '333';
+
+if(population > 33) {
+    console.log(`${country}'s population is above average.`)
+} else {
+    console.log(`${country}'s population is ${33 - population} million below average.`)
+}
+
+////////////////////////////////////
+// LECTURE: Type Conversion and Coercion
+
+console.log('9' - '5') // 4
+console.log('19' - '13' + '17') // '617'
+// minus operator converts '19' and '13' to numbers; 19 - 13 = 6
+// 6 converted to '6' by plus operator; '6' + '17' = '617'
+console.log('19' - '13' + 17) // 23
+console.log('123' < 57) // false
+console.log(5 + 6 + '4' + 9 - 4 - 2) // 1143
+// 11 converted to '11' by plus operator; '11' + '4' is '114'
+// '114' + '9' (9 is converted by plus operator) is '1149'
+// '1149' converted to 1149 by minus operator; 1149 - 4 = 1145
+// 1145 - 2 = 1143
+
+////////////////////////////////////
+// LECTURE: Equality Operators: == vs. ===
+
+const numNeighbors = Number(prompt('How many neighbour countries does your country have?'));
+
+if (numNeighbors === 1) {
+    console.log('Only 1 border!');
+} else if (numNeighbors > 1) {
+    console.log('More than 1 border.');
+} else {
+    console.log('No borders.')
+}
+
+////////////////////////////////////
+// LECTURE: Logical Operators
+
+const country = 'The United States of America'
+const language = 'English';
+let population = 333;
+isIsland = false;
+
+if (language === 'English' && population < 50 && !isIsland) {
+console.log(`You should live in ${country}. :)`);
+} else {
+console.log(`${country} does not meet your criteria. :(`);
+}
+
+////////////////////////////////////
+// LECTURE: The switch Statement
+
+const language = 'English';
+
+switch(language) {
+    case 'Chinese':
+    case 'Mandarin':
+        console.log('MOST number of native speakers!');
+        break;
+    case 'Spanish':
+        console.log('2nd place in number of native speakers');
+        break;
+    case 'English':
+        console.log('3rd place');
+        break;
+    case 'Hindi':
+        console.log('Number 4');
+        break;
+    case 'Arabic':
+        console.log('5th most spoken language');
+        break;
+    default:
+        console.log('Great language too :D');
+}
+
+////////////////////////////////////
+// LECTURE: The Conditional (Ternary) Operator
+
+const country = 'The United States of America'
+const population = 333;
+console.log(population);
+
+console.log(`${country}'s population is ${population > 33 ? 'above' : 'below'} average.`);
 */
--- 01-fundamentals-part-1/script.js
+++ 01-fundamentals-part-1/script.js
@@ -97,7 +97,6 @@ console.log(ageSarah >= 18);
 const isFullAge =  ageSarah >= 18;
 
 console.log(now - 1991 > now - 2018);
-*/
 
 ////////////////////////////////////
 // Operator Precedence
@@ -113,3 +112,285 @@ console.log(x, y);
 
 const averageAge = (ageJonas + ageSarah) / 2;
 console.log(ageJonas, ageSarah, averageAge);
+
+////////////////////////////////////
+// Coding Challenge #1
+
+const markMass = 78;
+const markHeight = 1.69;
+const johnMass = 92;
+const johnHeight = 1.95;
+
+const markBMI = markMass / markHeight ** 2;
+const johnBMI = johnMass / johnHeight ** 2;
+const markHigherBMI = markBMI > johnBMI;
+
+console.log(markBMI, johnBMI, markHigherBMI);
+
+// Strings and Template Literals
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthYear = '1991';
+const year = 2037;
+
+const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + ' year-old ' + job + '!'
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year-old ${job}!`;
+console.log(jonasNew);
+
+console.log(`Just a regular string...`);
+
+console.log('String with \n\
+multiple \n\
+lines');
+
+console.log(`String with
+multiple
+lines`);
+
+////////////////////////////////////
+// Taking Decisions: if / else Statements
+const age = 15;
+
+if (age >= 18) {
+    console.log('Sarah can start driving. 🚗')
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young to drive. Wait another ${yearsLeft} years. :)`)
+}
+
+const birthYear = 2012;
+
+let century;
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+console.log(century);
+
+////////////////////////////////////
+// Coding Challenge #2
+
+const markMass = 78;
+const markHeight = 1.69;
+const johnMass = 92;
+const johnHeight = 1.95;
+
+const markBMI = markMass / markHeight ** 2;
+const johnBMI = johnMass / johnHeight ** 2;
+
+if (markBMI > johnBMI) {
+    console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI})!`)
+} else {
+    console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI}!`)
+}
+
+////////////////////////////////////
+// Type Conversion and Coercion
+
+// type conversion
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Jonas'));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// type coercion
+console.log('I am ' + 23 + ' years old.')
+console.log('23' - '10' - 3);
+console.log('23' * '2');
+
+let n = '1' + 1; // '11'
+n = n - 1;
+console.log(n);
+
+////////////////////////////////////
+// Truthy and Falsy Values
+
+//5 falsy values: 0, '', undefined, null, NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 100;
+if (money) {
+    console.log("Don't spend it all.")
+} else {
+    console.log('You should  get a job!')
+}
+
+let height= 0;
+if (height) {
+    console.log('YAY! Height is defined')
+} else {
+    console.log('Height is UNDEFINED')
+}
+
+////////////////////////////////////
+// Equality Operators: == vs. ===
+
+const age = '18';
+if (age === 18) console.log('You just became an adult. (strict)');
+
+if (age == 18) console.log('You just became an adult. (loose)');
+
+const favorite = Number(prompt("What's your favorite number?"));
+console.log(favorite);
+console.log(typeof favorite);
+
+if (favorite === 23) { // 22 === 23 -> FALSE
+    console.log('Cool! 23 is an amazing number!')
+} else if (favorite === 7) {
+    console.log('7 is also a cool number.')
+} else if (favorite === 9){
+    console.log('9 is also a cool number.')
+} else {
+    console.log('Number is not 23, 7, or 9.')
+}
+
+if(favorite !== 23) console.log('Why not 23?');
+
+////////////////////////////////////
+// Logical Operators
+
+const hasDriversLicense = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+// if (hasDriversLicense && hasGoodVision) {
+//     console.log('Sarah is able to drive!')
+// } else {
+//     console.log('Someone else should drive...')
+// }
+
+const isTired = true; // C
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+    console.log('Sarah is able to drive!')
+} else {
+    console.log('Someone else should drive...')
+}
+
+////////////////////////////////////
+// Coding Challenge #3
+
+const scoreDolphins = (97 + 112 + 101) / 3;
+const scoreKoalas = (109 + 95 + 106) / 3;
+console.log(scoreDolphins, scoreKoalas);
+
+// if (scoreDolphins > scoreKoalas) {
+//     console.log('Dolphins win the trophy!')
+// } else if (scoreDolphins < scoreKoalas) {
+//     console.log('Koalas win the trophy!')
+// } else if (scoreDolphins === scoreKoalas) {
+//     console.log('Both win the trophy!')
+// }
+
+// Bonus
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+    console.log('Dolphins win the trophy!')
+} else if (scoreDolphins < scoreKoalas && scoreKoalas >= 100) {
+    console.log('Koalas win the trophy!')
+} else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreKoalas >= 100) {
+    console.log('Both teams win the trophy!')
+} else {
+    console.log('No team wins the trophy.')
+}
+
+////////////////////////////////////
+// The switch Statement
+
+const day = 'Monday';
+
+switch(day) {
+    case 'Monday': // day === 'Monday'
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+    case 'Tuesday':
+        console.log('Prepare theory videos')
+        break;
+    case 'Wednesday':
+    case 'Thursday':
+        console.log('Write code examples');
+        break;
+    case 'Friday':
+        console.log('Record videos')
+        break;
+    case 'Saturday':
+    case 'Sunday':
+        console.log('Enjoy the weekend');
+        break;
+    default:
+        console.log('Not a valid day')
+}
+
+if (day === 'Monday') {
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+} else if (day === 'Tuesday') {
+    console.log('Prepare theory videos')
+} else if(day === 'Wednesday' || day === 'Thursday') {
+    console.log('Write code examples');
+} else if (day === 'Friday') {
+    console.log('Record videos')
+} else if (day === 'Saturday' || day === 'Sunday') {
+    console.log('Enjoy the weekend');
+}  else {
+    console.log('Not a valid day')
+}
+
+////////////////////////////////////
+// Statements and Expressions
+
+3 + 4
+1991
+true && false && !false
+
+
+if (23 > 10) {
+    const str = '23 is bigger';
+}
+
+const me = 'Jonas';
+console.log(`I'm ${2037 - 1991} years old ${me}`)
+
+////////////////////////////////////
+// The Conditional (Ternary) Operator
+
+const age = 23;
+// age >= 21 ? console.log('I like to drink wine') : console.log('I like to drink water');
+
+const drink = age >= 21 ? 'wine' : 'water';
+console.log(drink);
+
+let drink2
+if (age >= 21) {
+    drink2 = 'wine';
+} else {
+    drink2 = 'water';
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 21 ? 'wine' : 'water'}`)
+
+////////////////////////////////////
+// Coding Challenge #4
+
+const bill = 275;
+
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+console.log(`The bill was $${bill}, the tip was $${tip}, and the total value was $${bill + tip}.`);
+*/
